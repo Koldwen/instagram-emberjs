096293ccd271b2fba163139c0453092ec2be9979
Andrés Carrasco <43501867+Koldwen@users.noreply.github.com>
2024-05-22 17:30:24 +0200

    refactor: Refactored code

--- app/components/left-side-panel/item/more/pop-up/component.js
+++ app/components/left-side-panel/item/more/pop-up/component.js
@@ -2,38 +2,41 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class MoreMenuComponent extends Component {
+export default class MorePopUpComponent extends Component {
   @tracked
   darkTheme = true;
+
+  get theme() {
+    return this.darkTheme ? 'dark' : 'light';
+  }
 
   @action
   toggleTheme() {
     this.darkTheme = !this.darkTheme;
-    let theme = this.darkTheme ? 'dark' : 'light';
 
     document.documentElement.style.setProperty(
       '--primary-color',
-      `var(--${theme}-primary-color)`,
+      `var(--${this.theme}-primary-color)`,
     );
     document.documentElement.style.setProperty(
       '--secondary-color',
-      `var(--${theme}-secondary-color)`,
+      `var(--${this.theme}-secondary-color)`,
     );
     document.documentElement.style.setProperty(
       '--tertiary-color',
-      `var(--${theme}-tertiary-color)`,
+      `var(--${this.theme}-tertiary-color)`,
     );
     document.documentElement.style.setProperty(
       '--quaternary-color',
-      `var(--${theme}-quaternary-color)`,
+      `var(--${this.theme}-quaternary-color)`,
     );
     document.documentElement.style.setProperty(
       '--color',
-      `var(--${theme}-color)`,
+      `var(--${this.theme}-color)`,
     );
     document.documentElement.style.setProperty(
       '--filter',
-      `var(--${theme}-filter)`,
+      `var(--${this.theme}-filter)`,
     );
   }
 }
